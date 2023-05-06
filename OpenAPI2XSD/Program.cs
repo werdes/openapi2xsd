8685117edb29da092d6f14c8217c1d7b171f59c5
Werdes <marius_noack@live.de>
2023-05-06 12:46:00 +0200

    Initial project + gitignore

---
+++ OpenAPI2XSD/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenAPI2XSD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
